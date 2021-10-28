24dffe579be8402042b8b831f76d67a4137e98ba
KushalS03 <90433697+KushalS03@users.noreply.github.com>
2021-10-28 15:25:25 -0600

    Add files via upload

---
+++ lecture_demos/src/main/java/LectureDemos.java
@@ -0,0 +1,8 @@
+public class LectureDemos {
+    static public void main(String[] args) {
+        Practise run = new Practise();
+
+        // Change this line to call the method you want to test
+        run.practise();
+    }
+}
---
+++ lecture_demos/src/main/java/Practise.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Practise {
+    Scanner input = new Scanner(System.in);
+
+    void practise() {
+
+
+    }
+}
